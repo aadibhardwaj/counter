83de9595a6d2fe146feb472ed8ec8c2c51494b39
Aadi <aadibhardwaj93@gmail.com>
2025-01-21 16:08:52 +0530

    first commit

--- vite-project/src/todo/home.jsx
+++ vite-project/src/todo/home.jsx
@@ -1,5 +1,18 @@
 function Home(){
+    const [inp,setInp] = useState("");
 
-    return <h1>ToDo App</h1>
+    // function changeInp(e){
+    //     setInp(e.target.value);
+    // }
+ return(
+    
+    <div id="todo">
+    <h1>ToDo App</h1>
+    <input type="text" placeholder="Enter ur Task" value={inp} onchange={changeIn}
+    <button>Add Task</button>
+    </div>
+
+    <ul></ul>
+ )
 }
 export default Home;
